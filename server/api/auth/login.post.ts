7d2f9be49e61238c6d0e2bce12f8c640464bb90b
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-05-27 12:35:49 -0300

    lint changes to endpoints

--- server/api/auth/login.post.ts
+++ server/api/auth/login.post.ts
@@ -1,15 +1,15 @@
 export default defineEventHandler(async (event) => {
-	const { email, password } = await readBody(event);
+  const { email, password } = await readBody(event)
 
-	// TODO: bcrypt password
-	if (email !== 'info@verifarma.com' || password !== '123321') {
-		return {
-			message: 'Email o contraseña incorrectos'
-		}
-	}
-	
-	return {
-	  message: 'Ingreso exitoso',
-	  token: 'abc-123'
-	}
-  })
+  // TODO: bcrypt password
+  if (email !== 'info@verifarma.com' || password !== '123321') {
+    return {
+      message: 'Email o contraseña incorrectos'
+    }
+  }
+
+  return {
+    message: 'Ingreso exitoso',
+    token: 'abc-123'
+  }
+})
--- server/api/auth/register.post.ts
+++ server/api/auth/register.post.ts
@@ -1,10 +1,11 @@
 export default defineEventHandler(async (event) => {
-	const { email, password } = await readBody(event);
+  const { email, password } = await readBody(event)
+  console.log(email, password)
 
-    // registerUser(email, password);
+  // registerUser(email, password);
 
-	// TODO: Add user in db
-	return {
-	  message: 'Usuario registrado con exito'
-	}
-  })
+  // TODO: Add user in db
+  return {
+    message: 'Usuario registrado con exito'
+  }
+})
