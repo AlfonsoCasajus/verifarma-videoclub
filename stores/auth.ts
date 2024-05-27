0aaab7b0046554c9bf32f3e7a9a54acaf9ac7de2
AlfonsoCasajus <alfonso.casajus@hotmail.com>
2024-05-27 12:35:29 -0300

    auth logic

--- stores/auth.ts
+++ stores/auth.ts
@@ -1,25 +1,39 @@
+interface AuthState {
+    token: null | string;
+}
+
 export const useAuthStore = defineStore('auth', {
-	state: () => ({
-	}),
-	getters: {
-	},
-	actions: {
-	  async login(email: string, password: string) {
-		const response: { message: string, token: string } = await $fetch('/api/auth/login', {
-			method: 'post',
-			body: { email, password }
-		});
+  state: (): AuthState => ({
+    token: null
+  }),
+  getters: {
+    getToken: state => state.token
+  },
+  actions: {
+    setAuthToken (token: string | null) {
+      this.token = token
+    },
+    async login (email: string, password: string) {
+      const response: { message: string, token: string } = await $fetch('/api/auth/login', {
+        method: 'post',
+        body: { email, password }
+      })
 
-		localStorage.setItem('auth-token', response.token);
-		
-		return { message: response.message, token: response.token };
-	  },
+      this.setAuthToken(response.token)
 
-	  async register(email: string, password: string) {
-		const response: any = await $fetch('/api/auth/register', {
-			method: 'post',
-			body: { email, password }
-		});
-	  },
-	},
-  })
+      return { message: response.message, token: response.token }
+    },
+
+    logout () {
+      this.setAuthToken(null)
+      localStorage.removeItem('auth-token')
+    },
+
+    async register (email: string, password: string) {
+      await $fetch('/api/auth/register', {
+        method: 'post',
+        body: { email, password }
+      })
+    }
+  }
+})
